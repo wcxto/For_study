fc2ed7adab667e9664e73c73ccde2f95956d766b
wcxto <163146366+wcxto@users.noreply.github.com>
2024-03-24 17:11:19 +0300

    Update 3 light.py

--- light.py
+++ light.py
@@ -16,3 +16,11 @@ for d in (dict_a,dict_b,dict_c):
 
 result = {**dict_a, **dict_a, **dict_c}
 print(result)
+
+
+a = [-5, -3, 1, 2, 3]
+new_a = sorted([i**2 for i in a])
+print(new_a)
+
+a = [-5, -3, 1, 2, 3]
+print(*a)
